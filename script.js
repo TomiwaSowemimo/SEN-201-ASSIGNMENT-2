c402e9c9966df74cdaec5bc959f56e86ed99da46
TomiwaSowemimo <tommzzy08@gmail.com>
2026-01-22 00:04:50 +0100

    Add files via upload
    Uploaded the html, css , and javascript files for the Project

---
+++ script.js
@@ -0,0 +1,7 @@
+function interested() {
+    alert("Thank you for your interest! An agent from EstateFlow will contact you shortly.");
+}
+
+// This automatically grabs the current year from the computer
+const yearSpan = document.getElementById('year');
+yearSpan.textContent = new Date().getFullYear();
